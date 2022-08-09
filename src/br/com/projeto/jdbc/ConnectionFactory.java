4b4b89d1bf935d9456bf263f621bdd01fe1c921f
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-08-08 23:52:34 -0300

    Criando a conexão com banco de dados

---
+++ src/br/com/projeto/jdbc/ConnectionFactory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class ConnectionFactory {
+    
+    public Connection getConnection() {
+        
+        try {
+            
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/bdvendas", "usuariocurso", "123");
+            
+        } catch(Exception erro) {
+            
+            throw new RuntimeException(erro);
+            
+        }
+        
+        return null;
+    }
+    
+}
