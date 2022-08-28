c7f54db75e0bf1c12b162adb621cf6acb1601498
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-08-28 19:20:44 -0300

    Criando os métodos Alterar e Excluir na classe ClienteDAO

--- src/br/com/projeto/dao/ClienteDAO.java
+++ src/br/com/projeto/dao/ClienteDAO.java
@@ -61,12 +61,56 @@ public class ClienteDAO {
         
     }
     
-    public void alterarCliente() {
+    public void alterarCliente(Cliente obj) {
+        String sql = "UPDATE tb_clientes SET nome = ?, rg = ?, cpf = ?, email = ?, telefone = ?, celular = ?, cep = ?, endereco = ?, numero = ?"
+                + ", complemento = ?, bairro = ?, cidade = ?, estado = ? WHRE id = ?";
         
+        try {
+            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getUf());
+            stmt.setInt(14, obj.getId());
+            
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!");
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + erro.getMessage());
+        }
     }
     
-    public void excluirCliente() {
+    public void excluirCliente(Cliente obj) {
+        String sql = "DELETE FROM tb_clientes WHERE id = ?";
         
+        try {
+            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            
+            stmt.setInt(1, obj.getId());
+            
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Excluído com Sucesso!");
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir: " + erro.getMessage());
+        }
     }
     
     public List<Cliente> listarClientes() {
