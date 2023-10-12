7f4384df725c404631f37e54c311be46b7a68136
= <=>
2023-10-12 01:18:10 -0300

    21. Criando o Modelo de Funcionários

---
+++ src/br/com/projeto/model/Funcionario.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.model;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Funcionario extends Pessoa {
+    
+    private String senha;
+    private String cargo;
+    private String nivelAcesso;
+    
+    public String getSenha() {
+        return senha;
+    }
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public String getCargo() {
+        return cargo;
+    }
+    
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+    
+    public void setNivelAcesso(String nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+    
+}
